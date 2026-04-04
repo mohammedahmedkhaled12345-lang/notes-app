bd3272bec3262bfb888b2027ae2bf3e7fc7c79b3
mohammed <mohammedahmedkhaled12345@gmail.com>
2026-04-05 01:59:58 +0200

    finsh sheet buttm

--- lib/widght/add_model_bottomshet.dart
+++ lib/widght/add_model_bottomshet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/widght/custom_textfelid.dart';
+import 'package:notesapp/widght/custtom_bittom.dart';
 
 class AddNotesBottomShet extends StatelessWidget {
   const AddNotesBottomShet({super.key});
@@ -8,13 +9,18 @@ class AddNotesBottomShet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 16, right: 16),
-      child: Column(
-        children: [
-          SizedBox(height: 32),
-          CustomTextfelid(hint: 'Titel'),
-          SizedBox(height: 30),
-          CustomTextfelid(hint: 'Content', maxlines: 7),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 32),
+            CustomTextfelid(hint: 'Titel'),
+            SizedBox(height: 30),
+            CustomTextfelid(hint: 'Content', maxlines: 7),
+            SizedBox(height: 32),
+            CustomBottom(),
+            SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widght/custtom_bittom.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:notesapp/constans_file.dart';
+
+class CustomBottom extends StatelessWidget {
+  const CustomBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        color: KprimaryColore,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
