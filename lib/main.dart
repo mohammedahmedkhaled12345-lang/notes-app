73d54b1cff4b4d5b2ea9ca3c0d399d8205edbd43
mohammed <mohammedahmedkhaled12345@gmail.com>
2026-04-05 02:14:24 +0200

    create edit note view

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark,fontFamily: 'Poppins'),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
       home: NotesView(),
     );
   }
--- lib/widght/custom_ote_item.dart
+++ lib/widght/custom_ote_item.dart
@@ -1,49 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/widght/edit_view_bodt.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 16, right: 0),
-      decoration: BoxDecoration(
-        color: Colors.amber,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            contentPadding: const EdgeInsets.only(right: 10, left: 24),
-            title: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                'Flutter Tips',
-                style: TextStyle(color: Colors.black, fontSize: 25),
-              ),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16),
-              child: Text(
-                'Bulid Your Career With MEAKH ',
-                style: TextStyle(color: Colors.black.withOpacity(.7)),
-              ),
-            ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.delete,
-                color: Colors.black.withOpacity(.7),
-                size: 32,
-              ),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditNoteViewBody();
+            },
           ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24, bottom: 24, top: 40),
-            child: Text('3 Apr  , 2026', style: TextStyle(color: Colors.black)),
-          ),
-        ],
+        );
+      },
+      child: Container(
+        padding: EdgeInsets.only(left: 16, right: 0),
+        decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.only(right: 10, left: 24),
+              title: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'Flutter Tips',
+                  style: TextStyle(color: Colors.black, fontSize: 25),
+                ),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Text(
+                  'Bulid Your Career With MEAKH ',
+                  style: TextStyle(color: Colors.black.withOpacity(.7)),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black.withOpacity(.7),
+                  size: 32,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 24, bottom: 24, top: 40),
+              child: Text(
+                '3 Apr  , 2026',
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
