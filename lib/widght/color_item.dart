65e4ce492fab5490d5035ae811faff2b0db0f260
mohammed <mohammedahmedkhaled12345@gmail.com>
2026-04-10 07:37:43 +0200

    colors listviwe part 2

--- lib/widght/color_item.dart
+++ lib/widght/color_item.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
+  final bool isActive;
+  final Color color;
+  const ColorItem({super.key, required this.isActive, required this.color});
   @override
   Widget build(BuildContext context) {
-    // 3. هنا بنرسم الـ UI
-    return CircleAvatar(backgroundColor: Colors.blue);
+    return isActive
+        ? CircleAvatar(
+            radius: 36,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(radius: 33, backgroundColor: color),
+          )
+        : CircleAvatar(backgroundColor: color);
   }
 }
--- lib/widght/colors_listview.dart
+++ lib/widght/colors_listview.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/widght/color_item.dart';
 
-class ColorsListview extends StatelessWidget {
- 
+class ColorsListview extends StatefulWidget {
+  @override
+  State<ColorsListview> createState() => _ColorsListviewState();
+}
 
+class _ColorsListviewState extends State<ColorsListview> {
+  int curretindex = 0;
+  List<Color> colors = [
+    Color(0xffDBB1BC),
+    Color(0xffD3C4E3),
+    Color(0xff8F95D3),
+    Color(0xff82CFF2),
+    Color(0xff58504A),
+    Color(0xffC2B97F),
+    Color(0xff410B13),
+    Color(0xffCD5D67),
+    Color(0xffBA1F33),
+    Color(0xffF2F7F2),
+  ];
   @override
   Widget build(BuildContext context) {
     // 3. هنا بنرسم الـ UI
@@ -11,9 +27,15 @@ class ColorsListview extends StatelessWidget {
       height: 80,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 5,
+        itemCount: colors.length,
         itemBuilder: (context, index) {
-          return ColorItem();
+          return GestureDetector(
+            onTap: () {
+              curretindex = index;
+              setState(() {});
+            },
+            child: ColorItem(isActive: curretindex == index,color: colors[index],),
+          );
         },
       ),
     );
